b80bb3c5ac6a959c347cc840e39c8c39b69acba1
eldenhowes <williameldenhowes@gmail.com>
2019-10-02 11:25:00 +0100

    Tidied up with rubocop

--- lib/sudoku_validator.rb
+++ lib/sudoku_validator.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 def sudoku_validator(sudoku_board)
-    sudoku_board.each_with_index do |line, line_index|
-        return false if line.include?(0)
-        line.each_with_index do |num, num_index|
-            line_to_check_index = line_index + 1
-            while line_to_check_index < 9
-                if num == sudoku_board[line_to_check_index][num_index]
-                    return false
-                end
-                line_to_check_index += 1
-            end
-        end
+  sudoku_board.each_with_index do |line, line_index|
+    return false if line.include?(0)
+
+    line.each_with_index do |num, num_index|
+      line_to_check_index = line_index + 1
+      while line_to_check_index < 9
+        return false if num == sudoku_board[line_to_check_index][num_index]
+
+        line_to_check_index += 1
+      end
     end
-    true
+  end
+  true
 end
