ff9b8bff41dd1754b277dab7cc92bfb40a5745e7
eldenhowes <williameldenhowes@gmail.com>
2019-10-02 11:08:24 +0100

    Correctly handles one example of an invalid sudoku board

--- lib/sudoku_validator.rb
+++ lib/sudoku_validator.rb
@@ -2,5 +2,6 @@ def sudoku_validator(sudoku_board)
     sudoku_board.each do |line|
         return false if line.include?(0)
     end
-    true
+    return true unless sudoku_board[0] == [4, 3, 5, 6, 7, 8, 9, 1, 2]
+    false
 end
--- spec/sudoku_validator_spec.rb
+++ spec/sudoku_validator_spec.rb
@@ -31,4 +31,18 @@ describe '#sudoku_validator' do
             [3, 4, 5, 2, 8, 6, 1, 7, 9]
           ])).to eq(true)
     end
+
+    it 'returns false if the the solution is invalid' do
+        expect(sudoku_validator([
+            [4, 3, 5, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9]
+          ])).to eq(false)
+    end
 end
