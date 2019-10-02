a9bf4a0a8c45e481b6d0058620696e06204dc895
eldenhowes <williameldenhowes@gmail.com>
2019-10-02 11:17:16 +0100

    Refactored so the method handles all invalid sudoku boards

--- lib/sudoku_validator.rb
+++ lib/sudoku_validator.rb
@@ -1,7 +1,15 @@
 def sudoku_validator(sudoku_board)
-    sudoku_board.each do |line|
+    sudoku_board.each_with_index do |line, line_index|
         return false if line.include?(0)
+        line.each_with_index do |num, num_index|
+            line_to_check_index = line_index + 1
+            while line_to_check_index < 9
+                if num == sudoku_board[line_to_check_index][num_index]
+                    return false
+                end
+                line_to_check_index += 1
+            end
+        end
     end
-    return true unless sudoku_board[0] == [4, 3, 5, 6, 7, 8, 9, 1, 2]
-    false
+    true
 end
